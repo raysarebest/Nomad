46c58239021b5d211bd8ca90d9dc12c361f310f3
Michael Hulet <bestraysfan@me.com>
2016-06-18 01:19:12 -0500

    I think I actually got Firebase set up somehow

--- Nomad/MHAppDelegate.swift
+++ Nomad/MHAppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class MHAppDelegate: UIResponder, UIApplicationDelegate{
@@ -14,6 +15,7 @@ class MHAppDelegate: UIResponder, UIApplicationDelegate{
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool{
+        FIRApp.configure()
         // Override point for customization after application launch.
         return true
     }
